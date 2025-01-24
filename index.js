a461c9514f117d0194e4d922bc34e73ecbda7e4b
Cat333t <284.1izm@gmail.com>
2025-01-24 19:25:04 +0200

    changing the code to make it more readable

--- index.js
+++ index.js
@@ -1,10 +1,92 @@
 let question = document.querySelector(".question")
 let answers = document.querySelectorAll(".answer")
 let container_h3 = document.querySelector(".container-h3")
-let btn_start = document.querySelector(".btn-start")
 
-let main_container =  document.querySelector('.main')
-let start_container =  document.querySelector('.start')
+let main_container =  document.querySelector('.container-question')
+
+let console_html = document.querySelector(".consol")
+let title = document.querySelector(".title")
+let settingsBtn = document.querySelector(".settings-btn")
+let btn1 = document.querySelector(".btn1")
+let btn2 = document.querySelector(".btn2")
+let btn3 = document.querySelector(".btn3")
+let time
+let timeout
+
+function timerfn() {
+    time -= 1
+    console_html.innerHTML = `Час: ${time} секунд`
+    timer = setTimeout(timerfn, 1000)
+}
+
+settingsBtn.removeEventListener('click', click_btn1)
+settingsBtn.addEventListener("click", click_btn1)
+
+btn1.removeEventListener('click', click_btn)
+btn1.addEventListener("click", () => {click_btn(120)})
+
+btn2.removeEventListener('click', click_btn)
+btn2.addEventListener("click", () => {click_btn(60)})
+
+btn3.removeEventListener('click', click_btn)
+btn3.addEventListener("click", () => {click_btn(10)})
+
+function click_btn1() {
+    console_html.innerHTML = 'Налаштування'
+    title.style.display = 'block'
+    title.innerHTML = 'Виберить час:'
+    title.innerHTML = 'Вибрати час'
+    settingsBtn.style.display = 'none'
+    btn1.style.display = 'block'
+    btn2.style.display = 'block'
+    btn3.style.display = 'block'   
+}
+function click_btn(input_time=30) {
+    time = input_time
+
+    console_html.innerHTML = 'Запуск...'
+    title.style.display = 'none'
+    settingsBtn.innerHTML = 'Запуск'
+    settingsBtn.style.paddingLeft = '50px'
+    settingsBtn.style.paddingRight  = '50px'
+    settingsBtn.style.display = 'block'
+    btn1.style.display = 'none'
+    btn2.style.display = 'none'
+    btn3.style.display = 'none'
+
+    settingsBtn.addEventListener("click", click_btn3)
+}
+
+function click_btn3() {
+        console_html.innerHTML = `Час: ${time} секунд`
+        document.querySelector('.container-main').style.display = 'none'
+        main_container.style.display = 'flex'
+
+        total_answers_given = 0
+        total_correct_answers = 0
+
+        current_question = new Question()
+        current_question.show()
+
+        timer = setTimeout(timerfn, 1000)
+
+        setTimeout(() => {
+            clearTimeout(timer)
+            console_html.innerHTML = `Ви дали ${total_correct_answers} правильних відповідей.<br>
+                Це ${roundNum(total_correct_answers * 100 / total_answers_given)}%`
+
+            question.style.display = 'none'
+            document.querySelector('.container-main').style.display = 'none'
+            settingsBtn.style.display = 'block'
+            settingsBtn.innerHTML = 'Заново' 
+            main_container.style.display = 'none'  
+
+            settingsBtn.removeEventListener('click', click_btn3)
+            settingsBtn.addEventListener("click", click_btn1)
+        }, (time * 1000))
+    }
+
+btn1.addEventListener("click", click_btn1)
 
 function randint(min, max) {
     return Math.round(Math.random() * (max-min) + min)
@@ -74,24 +156,6 @@ let current_question
 
 
 
-btn_start.addEventListener("click", () => {
-    
-    main_container.style.display = 'flex'
-    start_container.style.display = 'none'
-
-    
-
-    current_question = new Question()
-    current_question.show()
-
-    setTimeout(() => {
-        container_h3.innerHTML = `Ви дали ${total_correct_answers} правильних відповідей.<br>
-        Це ${roundNum(total_correct_answers * 100 / total_answers_given)}%`
-        start_container.style.display = 'flex'
-        main_container.style.display = 'none'
-        container_h3.style.fontSize = '24px'
-    }, [10000])
-})
 
 for(let i =0; i<answers.length; i++) {
     answers[i].addEventListener("click", () => {
@@ -102,7 +166,7 @@ for(let i =0; i<answers.length; i++) {
                 targets: answers[i],
                 delay: 100,
                 duration: 500,
-                background: '#fff',
+                background: '#000000',
                 easing: 'linear'
             })
         } else {
@@ -111,7 +175,7 @@ for(let i =0; i<answers.length; i++) {
                 targets: answers[i],
                 delay: 100,
                 duration: 500,
-                background: '#fff',
+                background: '#000000',
                 easing: 'linear'
             })
         }
